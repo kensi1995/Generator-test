cd35e8f3ea5df1a3606d47b7773560e7fd346a98
Kenan Novalić <kenannovalic2@gmail.com>
2024-11-25 11:47:00 +0100

    loadiIMg-edit

--- scripts.js
+++ scripts.js
@@ -1,18 +1,35 @@
 function preloadImages() {
+  const imagePromises = [];
+
   surveyData.forEach((data) => {
     if (data.options) {
       data.options.forEach((option) => {
         if (option.imgSrc) {
           const img = new Image();
           img.src = option.imgSrc;
-          img.onerror = () =>
-            console.error(`Failed to load image: ${option.imgSrc}`);
+
+          const imageLoadPromise = new Promise((resolve, reject) => {
+            img.onload = () => resolve(img);
+            img.onerror = () =>
+              reject(new Error(`Failed to load image: ${option.imgSrc}`));
+          });
+
+          imagePromises.push(imageLoadPromise);
         }
       });
     }
   });
-}
 
+  // Wait until all images are loaded before proceeding
+  Promise.all(imagePromises)
+    .then(() => {
+      console.log("All images loaded");
+      loadQuestion(); // Now load the question after preloading images
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 // Initialize the survey questions and options
 const surveyData = [
   {

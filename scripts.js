d383b5a1d95a1d3e33226a37bc85159f9f196286
Kenan Novalić <kenannovalic2@gmail.com>
2024-11-29 19:32:44 +0100

    language-edit

--- scripts.js
+++ scripts.js
@@ -15,45 +15,45 @@ function preloadImages() {
 // Initialize the survey questions and options
 const surveyData = [
   {
-    question: "What renovation measures are you planning in the near future?",
+    question: "Welche Sanierungsmaßnahmen planen Sie in naher Zukunft?",
     options: [
       {
-        title: "Roof insulation",
+        title: "Dachdämmung",
         imgSrc: "Icons/roof.webp",
       },
-      { title: "Window replacement", imgSrc: "Icons/replacement.webp" },
-      { title: "Façade insulation", imgSrc: "Icons/construction.webp" },
+      { title: "Fenstertausch", imgSrc: "Icons/replacement.webp" },
+      { title: "Fassadendämmung", imgSrc: "Icons/construction.webp" },
       {
-        title: "Heating replacement",
+        title: "Heizungstausch",
         imgSrc: "Icons/system.webp",
       },
       {
-        title: "Floor or basement ceiling insulation",
+        title: "Geschossdecke oder Kellerdecke",
         imgSrc: "Icons/basement.webp",
       },
-      { title: "Heating optimization", imgSrc: "Icons/heater.webp" },
+      { title: "Heizungsopimierung", imgSrc: "Icons/heater.webp" },
     ],
   },
   {
-    question: "How many residential units does the building have?",
+    question: "Wie viele Wohneinheiten hat das Gebäude?",
     options: [
-      { title: "1-2 units", imgSrc: "Icons/1-2units.webp" },
-      { title: "3-5 units", imgSrc: "Icons/3-5units.webp" },
-      { title: "5-10 units", imgSrc: "Icons/5-10units.webp" },
-      { title: "10-20 units", imgSrc: "Icons/10-20units.webp" },
-      { title: "More than 20 units", imgSrc: "Icons/apartments.webp" },
+      { title: "1-2 WE", imgSrc: "Icons/1-2units.webp" },
+      { title: "3-5 WE", imgSrc: "Icons/3-5units.webp" },
+      { title: "5-10 WE", imgSrc: "Icons/5-10units.webp" },
+      { title: "10-20 WE", imgSrc: "Icons/10-20units.webp" },
+      { title: "+20 WE", imgSrc: "Icons/apartments.webp" },
     ],
   },
   {
-    question: "Is the building detached or attached?",
+    question: "Ist das Gebäude Freistehend oder Angebaut?",
     options: [
-      { title: "Detached", imgSrc: "Icons/homeAlone.webp" },
-      { title: "Attached", imgSrc: "Icons/housetwo.webp" },
+      { title: "nein", imgSrc: "Icons/homeAlone.webp" },
+      { title: "Ja", imgSrc: "Icons/housetwo.webp" },
     ],
   },
 
   {
-    question: "Year of construction of the house?",
+    question: "Baujahr Haus?",
     options: [
       { title: "Before 1920", imgSrc: "Icons/calendar.webp" },
       { title: "1920-1950", imgSrc: "Icons/calendar.webp" },
@@ -63,36 +63,36 @@ const surveyData = [
     ],
   },
   {
-    question: "What heating system is installed?",
+    question: "Welches Heizsystem ist verbaut?",
     options: [
-      { title: "Oil", imgSrc: "Icons/oil-barrel.webp" },
+      { title: "Öl", imgSrc: "Icons/oil-barrel.webp" },
       { title: "Gas", imgSrc: "Icons/gas.webp" },
-      { title: "Wood", imgSrc: "Icons/wood.webp" },
+      { title: "Holz", imgSrc: "Icons/wood.webp" },
     ],
     type: "withInput",
   },
   {
-    question: "Have any renovation measures already been carried out?",
+    question: "Wurden bereits Sanierungsmaßnahen durchgeführt?",
     options: [
       {
-        title: "Roof insulation",
+        title: "Dachdämmung",
         imgSrc: "Icons/roof.webp",
       },
-      { title: "Window replacement", imgSrc: "Icons/replacement.webp" },
-      { title: "Façade insulation", imgSrc: "Icons/construction.webp" },
+      { title: "Fenstertausch", imgSrc: "Icons/replacement.webp" },
+      { title: "Fassadendämmung", imgSrc: "Icons/construction.webp" },
       {
-        title: "Heating replacement",
+        title: "Heizungstausch",
         imgSrc: "Icons/system.webp",
       },
       {
-        title: "Floor or basement ceiling insulation",
+        title: "Geschossdecke oder Kellerdecke",
         imgSrc: "Icons/basement.webp",
       },
-      { title: "Heating optimization", imgSrc: "Icons/heater.webp" },
+      { title: "Heizungsopimierung", imgSrc: "Icons/heater.webp" },
     ],
   },
   {
-    question: "How expensive will these measures be approximately?",
+    question: "Wir teuer werden diese Maßnahmen etwa sein (Schätzung)?",
     type: "slider",
     min: 0,
     max: 500000,
@@ -100,10 +100,10 @@ const surveyData = [
   },
 
   {
-    question: "Enter your address",
+    question: "Geben Sie Ihre Adresse ein",
     type: "map",
     inputField: true,
-    placeholder: "Enter your address",
+    placeholder: "Geben Sie Ihre Adresse ein",
   },
 ];
 
@@ -164,7 +164,7 @@ function renderOptionsQuestion(container, data) {
     const inputField = document.createElement("input");
     inputField.type = "text";
     inputField.id = "other-input";
-    inputField.placeholder = "Other (please specify)";
+    inputField.placeholder = "Sonstiges (bitte angeben)";
 
     const submitButton = document.createElement("button");
     submitButton.textContent = "Submit";
@@ -352,11 +352,11 @@ function showForm() {
   const questionContainer = document.getElementById("dynamic-container");
   questionContainer.innerHTML = `
   <div  class="formDivStyle fade-in"> <!-- Added the fade-in class here -->
-        <h2 class="form-title">Enter your information</h2>
+        <h2 class="form-title">Kontaktinformationen angeben</h2>
         <form id="user-form"class="formEditStyle" >
-          <input type="text" id="full-name" placeholder="Full Name" required><br>
-          <input type="email" id="email" placeholder="Email" required><br>
-          <input type="tel" id="phone" placeholder="Phone Number" required><br>
+          <input type="text" id="full-name" placeholder="Name" required><br>
+          <input type="email" id="email" placeholder="email" required><br>
+          <input type="tel" id="phone" placeholder="telefon" required><br>
           <button class="formBtn" type="submit">Submit</button>
         </form>
         </div>
@@ -369,14 +369,14 @@ function showForm() {
 //Format question in email
 function formatAnswers(answers) {
   const questionsMap = {
-    question_0: "What renovation measures are you planning in the near future?",
-    question_1: "How many residential units does the building have?",
-    question_2: "Is the building detached or attached?",
-    question_3: "Year of construction of the house?",
-    question_4: "What heating system is installed?",
-    question_5: "Have any renovation measures already been carried out?",
-    question_6: "How expensive will these measures be approximately?",
-    question_7: "Enter your address",
+    question_0: "Welche Sanierungsmaßnahmen planen Sie in naher Zukunft?",
+    question_1: "Wie viele Wohneinheiten hat das Gebäude?",
+    question_2: "Ist das Gebäude Freistehend oder Angebaut?",
+    question_3: "Baujahr Haus?",
+    question_4: "Welches Heizsystem ist verbaut?",
+    question_5: "Wurden bereits Sanierungsmaßnahen durchgeführt?",
+    question_6: "Wir teuer werden diese Maßnahmen etwa sein (Schätzung)?",
+    question_7: "Geben Sie Ihre Adresse ein",
   };
 
   let formattedAnswers = "";
